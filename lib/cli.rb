a0d6bc712c8f9ec56a683596b62334af2ef4bec8
Kat Leight <leightkt@gmail.com>
2021-01-06 13:53:08 -0700

    added view hike details feature

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,6 +8,7 @@ class Cli
     end
 
     def welcome
+        clear
         puts "Welcome to HikeSelector!"
         name = prompt.ask("What is your name?")
         puts "Hi #{name}!"
@@ -16,7 +17,26 @@ class Cli
     def invitation
         choices = ["Location", "Dog Friendly?", "Elevation Gain", "Difficulty", "Distance"]
         answer = prompt.select("How would you like to search for a hike?", choices)
-        filter_hikes(answer)
+        hikes = filter_hikes(answer)
+        see_hike_details(hikes)
+        continue_or_exit
+    end
+
+    def continue_or_exit
+        choices = ["Look for another hike", "Exit"]
+        answer = prompt.select("Would you like to look for another hike or exit?", choices)
+        if answer == "Exit"
+            exit
+        else
+            clear
+            invitation
+        end
+    end
+
+    def see_hike_details(hikes)
+        hike = prompt.select("Select a hike to view more details:", hikes.pluck(:name))
+        hike = Hike.find_by(name: hike)
+        puts "NAME: #{hike.name}\nLOCATION: #{hike.location}\nDOG FRIENDLY?: #{hike.dog_friendly}\nDISTANCE: #{hike.distance}\nELEVATION GAIN: #{hike.elevation}\nDIFFICULTY: #{hike.difficulty}"
     end
 
     def list_hikes
@@ -37,13 +57,17 @@ class Cli
             choices = ["Beginner", "Intermediate", "Advanced"]
             selection = prompt.select("How hard do you want it?", choices)
             Hike.difficulty(selection)
-        when "Dog Friendly"
+        when "Dog Friendly?"
             Hike.dog_friendly?
         when "Location"
             choices = Hike.unique_locations
-            location = prompt.select("Where do you want it?", choices)
+            location = prompt.select("Where do you want it?", choices, filter: true) 
             Hike.location(location)
         end
+    end
+
+    def clear
+        system 'clear'
     end
 
 end
--- runner.rb
+++ runner.rb
@@ -4,8 +4,9 @@ require_relative 'config/environment'
 
 app = Cli.new
 
-# app.welcome
+app.welcome
 
 app.invitation
 
-#display choices based on filters
+# add favs for users
+# user login
